9fc74c34cae310373d2c6e201b65928973ac4000
dev.sanjeetsahu <sanjeetsahus29@gmail.com>
2026-09-09 14:41:18 +0530

    intialise a react project with vite bundler and add a tailwindcss and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
